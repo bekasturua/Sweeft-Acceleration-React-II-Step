0f36e170b1d5bb6ef504f784f7c80f7ea41452a0
Beka Sturua <beqasturua3@gmail.com>
2022-07-14 01:57:20 +0400

    add  user info css

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,42 +3,61 @@ import img from "../User/img.jpg";
 
 const User = (props) => {
   return (
-    <div className={classes.body}>
-      <div className={classes.header}>
+    <div className={classes.header}>
+      <img src={img} alt="profile" />
+      <fieldset className={classes.info}>
+        <legend>Info</legend>
         <div>
-          <img className={classes.img} src={img} alt="..." />
-          <fieldset className={classes.info}>
-            <legend>info</legend>
-            <div className={classes.list}>
-              <div>
-                <strong>prefix, name, lastName</strong>
-              </div>
-              <div>title</div>
-              <br />
-              <div>Email:</div>
-              <div>Ip Address:</div>
-              <div>Ip Address:</div>
-              <div>Job Area:</div>
-              <div>Job Type:</div>
-            </div>
-          </fieldset>
-          <fieldset className={classes.address}>
-            <legend>Address</legend>
-            <div>
-              <strong>prefix, name, lastName</strong>
-            </div>
-            <div>City:</div>
-            <div>Country:</div>
-            <div>State:</div>
-            <div>Street Adress:</div>
-            <div>Zip:</div>
-          </fieldset>
+          <strong>Hi</strong>
         </div>
-      </div>
-      <div>
-        <div className={classes.space}></div>
-        <h2>Friends:</h2>
-      </div>
+        <div>
+          <i>Bye</i>
+        </div>
+        <br />
+        <div>
+          <span>email: </span>
+          beqasturua3@gmail.com
+        </div>
+        <div>
+          <span>Ip Address: </span>
+          Tbilisi
+        </div>
+        <div>
+          <span>Ip Address: </span>
+          234235325
+        </div>
+        <div>
+          <span>Job Area: </span>
+          Front-end
+        </div>
+        <div>
+          <span>Job Type: </span>
+          front
+        </div>
+      </fieldset>
+
+      <fieldset className={classes.addressInfo}>
+        <legend>Address</legend>
+        <div>
+          <strong>hello</strong>
+          <div>
+            <span>City: </span> tbilisi
+          </div>
+          <div>
+            <span>Country: </span> georgia
+          </div>
+          <div>
+            <span>State: </span> georgia
+          </div>
+          <div>
+            <span>Street Address: </span>
+            tbilisi
+          </div>
+          <div>
+            <span>ZIP: </span>0103
+          </div>
+        </div>
+      </fieldset>
     </div>
   );
 };
